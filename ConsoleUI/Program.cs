3e8fd4f601959190449b0ec74c2de852282f3988
Turan <info@yennimusic.com>
2021-11-15 00:26:25 +0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProductManager productManager = new ProductManager(new EfProductDal());
+			foreach (Product product in productManager.GetAllByUnitPrice(50,100))
+			{
+				Console.WriteLine(product.ProductName);
+			}
+		}
+	}
+}
---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+	public class NorthwindContext:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=DESKTOP-62OF0BB;Database=Northwind;Trusted_Connection=true");
+		}
+		DbSet<Product> Products { get; set; }
+		DbSet<Category> Categories { get; set; }
+		DbSet<Customer> Customers { get; set; }
+	}
+}
