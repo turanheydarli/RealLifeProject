996bbae64bd1e97b6c0ed16459bf33d7970dadb3
Turan <info@yennimusic.com>
2021-11-15 18:03:57 +0400

    Entity Base

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
 	//IEntity implement edən class bir databaza cədvəlidir. Çıplak olmasın deyə veririk.
 	public interface IEntity
